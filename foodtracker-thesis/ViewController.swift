ca1b25f41c019fc6193d74fee0abde13e9e0542d
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2021-12-16 09:56:15 +0700

    Initial Commit

---
+++ foodtracker-thesis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodtracker-thesis
+//
+//  Created by Audrey Aurelia Chang on 16/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
