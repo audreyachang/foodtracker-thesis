cd61c7f78324067b86a226e36cfb3fff8f0f1695
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2022-01-11 20:27:14 +0700

    Added reusable components
    Added reusable components for displays such as views including labels; date pickers and text fields
    Added visual components on Food Form
    Added search bar and notification button on Food Tracker

---
+++ foodtracker-thesis/Displays/Global Components/LabelWithDate/LabelWithDate.swift
@@ -0,0 +1,20 @@
+//
+//  LabelWithDate.swift
+//  foodtracker-thesis
+//
+//  Created by Audrey Aurelia Chang on 10/01/22.
+//
+
+import UIKit
+
+class LabelWithDate: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ foodtracker-thesis/Displays/Global Components/LabelWithTextField/LabelWithTextField.swift
@@ -0,0 +1,20 @@
+//
+//  LabelWithTextField.swift
+//  foodtracker-thesis
+//
+//  Created by Audrey Aurelia Chang on 10/01/22.
+//
+
+import UIKit
+
+class LabelWithTextField: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
