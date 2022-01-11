cf86ad6e674d7c0ceed217960ace8aab61c906b0
Audrey Aurelia Chang <audreyaureliachang@gmail.com>
2022-01-11 20:51:27 +0700

    Added delegate logic on Label with text field
    Added delegate logic on custom component label with text field

--- foodtracker-thesis/Displays/Global Components/LabelWithTextField/LabelWithTextField.swift
+++ foodtracker-thesis/Displays/Global Components/LabelWithTextField/LabelWithTextField.swift
@@ -12,7 +12,9 @@ protocol LabelWithTextFieldDelegate: AnyObject{
 }
 
 class LabelWithTextField: UIView {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     weak var delegate: LabelWithTextFieldDelegate?
     
     func setUp(dlgt: LabelWithTextFieldDelegate){
@@ -27,10 +29,49 @@ class LabelWithTextField: UIView {
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
         initWithNib()
+        textField.delegate = self
+        setupStyle()
+    }
+    
+    convenience init(title: String, textFieldPh: String, textFieldData: String?){
+        self.init()
+        titleLabel.text = title
+        textField.placeholder = textFieldPh
+        textField.text = textFieldData
+        textField.delegate = self
+    }
+    
+    fileprivate func initWithNib(){
+        guard let view = loadViewFromNib(nibName: "LabelWithTextField")else {return}
+        view.frame = self.bounds
+        self.addSubview(view)
         
     }
     
-    func initWithNib(){
+    func loadViewFromNib(nibName: String)->UIView?{
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: nibName, bundle: bundle)
         
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+}
+
+extension LabelWithTextField: UITextFieldDelegate{
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.text?.count ?? 0 > 4 {
+            delegate?.isTextFieldFilled(was: true)
+        }else{
+            delegate?.isTextFieldFilled(was: false)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.endEditing(true)
+        return false
+    }
+    
+    func setupStyle(){
+        textField.tintColor = .black
+        textField.layer.cornerRadius = 5
     }
 }
